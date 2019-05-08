6d65dc930868ff71d9595a55a1f3e91df904dacd
sofia_f <lx.sonik.xl@gmail.com>
2019-05-08 21:29:06 +0300

    5.2

---
+++ 5.2 to do list/src/Loader.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Loader {
+    static ArrayList<String> toDoList = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            String input = scanner.nextLine();
+            
+
+
+        }
+    }
+
+    public static void list(String input){
+
+    }
+
+    public static void add(String input){
+
+    }
+
+    public static void delete(String input){
+
+    }
+
+    public static void edit(String input){
+
+    }
+}
