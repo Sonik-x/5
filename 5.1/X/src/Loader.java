8468228d5cf8131dcf48c351a8819f910dcf6125
sofia_f <lx.sonik.xl@gmail.com>
2019-05-08 21:20:50 +0300

    foreach
    Вынесла печать в отдельный метод, добавила два цикла foreach, один из них оказался нерабочим - пришлось изучать вопрос

--- 5.1/X/src/Loader.java
+++ 5.1/X/src/Loader.java
@@ -2,20 +2,20 @@ public class Loader {
     public static void main(String[] args) {
 
         var X = createX(25);
-
-        for (int i = 0; i < X.length; i++) {
-            System.out.println();
-            for (int j = 0; j < X[i].length; j++) {
-                System.out.print(X[i][j]);
-            }
-        }
+        printArreyOfArreys(X);
 
     }
 
     public static String[][] createX(int size) {
         String[][] result = new String[size][size];
 
-        for (int i = 0; i < size; i++) {
+        /* for (String[] xString : result) {      этот код не работает, потому что (внезапно) для каждой итерации создаётся новая ссылка,
+            for (String str : xString) {          и уже ей присваевается следующий объект. То есть присваивать что-то ей бесполезно. грусть.
+                str = " ";
+            }
+        } */
+
+        for (int i = 0; i < size; i++) {     //работает
             for (int j = 0; j < size; j++) {
                 result[i][j] = " ";
             }
@@ -27,6 +27,15 @@ public class Loader {
         }
 
         return result;
+    }
+
+    public static void printArreyOfArreys(Object[][] theArrey) {  //в процессе выяснилось, что для массива примитивов fori быстрее, чем foreach
+        for (Object[] line : theArrey) {                          //а для List быстрее foreach
+            System.out.println();
+            for (Object obj : line) {                             //что из этого быстрее в моём случае не очень понятно, но foreach красивее
+                System.out.print(obj);
+            }
+        }
     }
 
 }
