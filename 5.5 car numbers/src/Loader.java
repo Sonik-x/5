42e92f473f5b7fe9ad4eda708155b0536a5f15e3
sofia_f <lx.sonik.xl@gmail.com>
2019-05-24 23:26:32 +0300

    >.<

--- 5.5 car numbers/src/Loader.java
+++ 5.5 car numbers/src/Loader.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Loader {
 
@@ -7,14 +7,19 @@ public class Loader {
 
     public static ArrayList<String> numbersList = new ArrayList<>();
 
+    public static long startTime = 0;
+
+    public static HashSet<String> hashSet = new HashSet<>();
+    public static TreeSet<String> treeSet = new TreeSet<>();
 
     public static void main(String[] args) {
+
 
         for (int i = 0; i < charList.length; i++) {
             for (int j = 0; j <= 9; j++) {
                 for (int k = 1; k <= 197; k++) {
-                    String ks = ((Integer)k).toString();
-                    while (ks.length() < 3){
+                    String ks = ((Integer) k).toString();
+                    while (ks.length() < 3) {
                         ks = "0" + ks;
                     }
 
@@ -31,27 +36,53 @@ public class Loader {
             }
         }
 
+        treeSet.addAll(numbersList);
+        hashSet.addAll(numbersList);
 
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (true) {
+            String input = scanner.nextLine();
+            //String input = "F000AA001";
+
+            setStartTime();
+            System.out.println("Binary: " + findNumberBinary(input) + " " + duration());
+            setStartTime();
+            System.out.println("Hash: " + findNumberHash(input) + " " + duration());
+            setStartTime();
+            System.out.println("Straight: " + findNumberStraight(input) + " " + duration());
+            setStartTime();
+            System.out.println("Tree: " + findNumberTree(input) + " " + duration());
+
+        }
 
     }
 
-    public static boolean findNumberStraight(String number){
-
+    public static boolean findNumberStraight(String number) {
+        if (numbersList.contains(number)) {
+            return true;
+        }
         return false;
     }
 
-    public static boolean findNumberBinary(String number){
-
-        return false;
+    public static boolean findNumberBinary(String number) {
+        return Collections.binarySearch(numbersList, number) >= 0;      //в действительности отдаёт не -1, а некое отрицательное число
     }
 
-    public static boolean findNumberHash(String number){
-
-        return false;
+    public static boolean findNumberHash(String number) {
+        return hashSet.contains(number);
     }
 
-    public static boolean findNumberTree(String number){
+    public static boolean findNumberTree(String number) {
+        return treeSet.contains(number);
+    }
 
-        return false;
+    public static void setStartTime() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public static long duration() {
+        return System.currentTimeMillis() - startTime;
     }
 }
